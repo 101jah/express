0b2d11b12ac290fae4607ce02d59dcbefb1d12a3
101jah <101jah@proton.me>
2024-06-15 09:50:35 -0500

    Updated

--- myApp.js
+++ myApp.js
@@ -3,12 +3,10 @@ let app = express();
 const port = 3000; // or any port number you prefer
 const path = require("path");
 
-const absolutePath = "/views/index.html";
-const home = path.join(__dirname, absolutePath);
+home = __dirname + '/relativePath/file.ext'
 
-// Define a route handler for the root path '/'
-app.get("/", (req, res) => {
-  res.send(home);
-});
-
+// Define a route handler for the absolute path
+ app.get("/",req,res) {
+  res.sendFile(home);
+  };
 module.exports = app;
