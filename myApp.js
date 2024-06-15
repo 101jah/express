20ac299498dcf51d2b11f462f0673bc4e361be40
101jah <101jah@proton.me>
2024-06-15 09:31:12 -0500

    Updated

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require("express");
 let app = express();
 const port = 3000; // or any port number you prefer
+const path = require("path");
+
+const absolutePath = "/views/index.html";
+const home = path.join(__dirname, absolutePath);
 
 // Define a route handler for the root path '/'
 app.get("/", (req, res) => {
