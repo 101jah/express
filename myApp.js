6a22e8d168fbb1d517a36ca257a8033eaade19bf
101jah <101jah@proton.me>
2024-06-15 09:13:23 -0500

    Updated

--- myApp.js
+++ myApp.js
@@ -3,13 +3,8 @@ let app = express();
 const port = 3000; // or any port number you prefer
 
 // Define a route handler for the root path '/'
-app.get('/', (req, res) => {
-  res.send('Hello Express');
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.send("Hello Express");
 });
 
 module.exports = app;
