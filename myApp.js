bb10b7b1150440e6155e54165bf4a7d184601583
101jah <101jah@proton.me>
2024-06-15 16:37:59 -0500

    Updated

--- myApp.js
+++ myApp.js
@@ -3,10 +3,11 @@ let app = express();
 const port = 3000; // or any port number you prefer
 const path = require("path");
 
-home = __dirname + "/views/index.html";
+home = __dirname + "/public";
 
 // Define a route handler for the absolute pathably
 app.get("/", (req, res) => {
   res.sendFile(home);
 });
+app.use(home, express.static());
 module.exports = app;
